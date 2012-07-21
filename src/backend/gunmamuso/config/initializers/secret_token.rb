c145108e1cbf4da9e40c000c1a5fc60f7acb0272
KOMATSU Issei <issei@mizuhataya.com>
2012-07-21 19:16:14 +0900

    Rails追加

---
+++ src/backend/gunmamuso/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gunmamuso::Application.config.secret_token = '3c297942fc840e91d3e441eacc941cfe8570d834a58e6d3db167fe03e37337f177934ddcc8c718eff025979f75a9976faf777f92a779766cca902e5ff1102971'
